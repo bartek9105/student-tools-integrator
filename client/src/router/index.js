8069864efe79a1c29aa997d6cbae3d290c81649a
bartek9105 <bartek9105@gmail.com>
2020-04-23 10:28:53 +0200

    basic routing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Dashboard from '@/views/Dashboard'
 import Subjects from '@/views/Subjects'
+import Calendar from '@/views/Calendar'
+import Exams from '@/views/Exams'
+import Offers from '@/views/Offers'
+import Schedule from '@/views/Schedule'
+import Todo from '@/views/Todo'
 
 Vue.use(VueRouter)
 
@@ -15,6 +20,31 @@ const routes = [
     path: '/subjects',
     name: 'Subjects',
     component: Subjects
+  },
+  {
+    path: '/calendar',
+    name: 'Calendar',
+    component: Calendar
+  },
+  {
+    path: '/exams',
+    name: 'Exams',
+    component: Exams
+  },
+  {
+    path: '/offers',
+    name: 'Offers',
+    component: Offers
+  },
+  {
+    path: '/schedule',
+    name: 'Schedule',
+    component: Schedule
+  },
+  {
+    path: '/todo',
+    name: 'Todo',
+    component: Todo
   }
 ]
 
