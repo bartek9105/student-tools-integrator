a38bda8e5aee4c408549a6f094cc1b7db3f2baa8
bartek9105 <bartek9105@gmail.com>
2020-04-23 09:58:15 +0200

    add left navbar with menu, add material icons

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <v-app>
-    <Navbar/>
     <router-view/>
   </v-app>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar'
 
 export default {
-  name: 'App',
-  components: {
-    Navbar
-  }
+  name: 'App'
 }
 </script>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <Navbar/>
     <h1>Dashboard</h1>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
-  name: 'Home'
+  name: 'Dashboard',
+  components: {
+    Navbar
+  }
 }
 </script>
