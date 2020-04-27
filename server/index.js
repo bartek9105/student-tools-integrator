c980523e4ff0a49424ec5d436523c9e8e4de5b76
bartek9105 <bartek9105@gmail.com>
2020-04-27 20:24:59 +0200

    database connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('hello world')
+})
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-xbxsg.mongodb.net/student-integrator?retryWrites=true&w=majority`, {useNewUrlParser: true}, () => {
+    console.log('Connected to db')
 })
 
 app.listen(3000)
