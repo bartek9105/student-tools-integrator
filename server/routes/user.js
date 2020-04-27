731e076a3240f71f01a9a411c308e5824cb036ce
bartek9105 <bartek9105@gmail.com>
2020-04-27 20:34:28 +0200

    user controller initialization

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const userController = require('../controllers/User')
+
 router.get('/', (req, res) => {
     res.send('user endpoint')
 })
 
-router.get('/login', (req, res) => {
-    res.send('user login endpoint')
-})
+router.get('/login', userController.login)
 
 module.exports = router
