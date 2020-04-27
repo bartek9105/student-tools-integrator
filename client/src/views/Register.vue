2397adad691e7ed7be52e9ed374fc49644d6744e
bartek9105 <bartek9105@gmail.com>
2020-04-27 21:22:35 +0200

    register view

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,12 +1,49 @@
 <template>
-  <div>
-      <h1>Register</h1>
-  </div>
+  <v-form>
+    <v-container>
+      <v-row class="flex-column align-center">
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="password"
+            label="Password"
+            type="password"
+            required
+          ></v-text-field>
+        </v-col>
+        <p>Already registered?
+          <router-link to="login">
+            Login
+          </router-link>
+        </p>
+        <router-link to="/login">
+          <v-btn color="primary">Register</v-btn>
+        </router-link>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
 }
 </script>
 
