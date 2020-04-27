dc1248a27590bd5099edf358ea14fa80040b7383
bartek9105 <bartek9105@gmail.com>
2020-04-27 21:53:23 +0200

    basic register logic

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,3 +1,20 @@
+const User = require('../models/User')
+
+exports.register = async (req, res, next) => {
+    try {
+        const user = new User({
+            email: req.body.email,
+            password: req.body.password
+        })
+        const savedUser = await user.save()
+        res.send({
+            user: savedUser
+        })        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 exports.login = (req, res) => {
     res.send('login')
 }
