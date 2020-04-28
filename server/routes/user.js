cb73e8e37176ea22ccc3911c2a76c27466a25c42
bartek9105 <bartek9105@gmail.com>
2020-04-28 10:07:47 +0200

    add joi validation for login and register

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const userController = require('../controllers/User')
+const authController = require('../controllers/authController')
+const authPolicy = require('../policies/authPolicy')
 
-router.post('/register', userController.register)
+router.post('/register', authPolicy.authValidator, authController.register)
 
-router.post('/login', userController.login)
+router.post('/login', authPolicy.authValidator, authController.login)
 
 module.exports = router
