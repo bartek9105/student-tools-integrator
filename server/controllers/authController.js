9d5dcaf24e72e22963bb5a17b43fa6ce4e6b1b90
bartek9105 <bartek9105@gmail.com>
2020-04-28 11:00:56 +0200

    add jwt config and send token for logged user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,10 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 exports.register = async (req, res, next) => {
-    const hashPassword = await bcrypt.hash(req.body.password, 12)
     try {
+        const hashPassword = await bcrypt.hash(req.body.password, 12)
         const user = new User({
             email: req.body.email,
             password: hashPassword
@@ -17,6 +18,31 @@ exports.register = async (req, res, next) => {
     }
 }
 
-exports.login = (req, res) => {
-    res.send('login')
+exports.login = async (req, res) => {
+    try {
+        const user = await User.findOne({
+            email: req.body.email
+        })
+        if (!user) {
+            res.send({
+                error: 'User not found'
+            })
+        }
+        const isEqual = await bcrypt.compare(req.body.password, user.password)
+        if (!isEqual) {
+            res.send({
+                error: 'Wrong password'
+            })
+        }
+        const token = jwt.sign({
+            email: req.body.email,
+            userId: user._id
+        }, 'secretsecretsecretkey', { expiresIn: '1h'})
+        res.send({
+            token: token,
+            userId: user._id
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
