0305f11bcd81aff1d12eae80bcc49a3f091ae291
bartek9105 <bartek9105@gmail.com>
2020-05-07 13:34:23 +0200

    basic template structure

--- client/src/views/Subjects.vue
+++ client/src/views/Subjects.vue
@@ -1,11 +1,53 @@
 <template>
-  <v-container>
-  </v-container>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="10"
+        >
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            label="Subject name"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="2">
+          <v-btn small color="primary">Add</v-btn>
+        </v-col>
+      </v-row>
+      <v-card
+        class="mx-auto"
+        tile
+      >
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">Name</th>
+              <th class="text-left">Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in desserts" :key="item.name">
+              <td>{{ item.name }}</td>
+              <td>{{ item.calories }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+      </v-card>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
