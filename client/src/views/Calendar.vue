a10f36ac484634b20895c2f4bcc7b448e4a399bb
bartek9105 <bartek9105@gmail.com>
2020-05-13 22:12:29 +0200

    event details popup

--- client/src/views/Calendar.vue
+++ client/src/views/Calendar.vue
@@ -72,20 +72,20 @@
                   :color="selectedEvent.color"
                   dark
                 >
-                  <v-btn icon>
-                    <v-icon>mdi-pencil</v-icon>
-                  </v-btn>
+
                   <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
                   <v-spacer></v-spacer>
                   <v-btn icon>
-                    <v-icon>mdi-heart</v-icon>
-                  </v-btn>
-                  <v-btn icon>
-                    <v-icon>mdi-dots-vertical</v-icon>
+                    <v-icon @click="deleteEvent(selectedEvent._id)">mdi-delete</v-icon>
                   </v-btn>
                 </v-toolbar>
                 <v-card-text>
-                  <span v-html="selectedEvent.details"></span>
+                  <form v-if="editing !== selectedEvent._id">
+                    {{ selectedEvent.details }}
+                  </form>
+                  <form v-else>
+                    <textarea v-model="selectedEvent.details" type="text" style="width: 100%" placeholder="Add note"></textarea>
+                  </form>
                 </v-card-text>
                 <v-card-actions>
                   <v-btn
@@ -94,6 +94,9 @@
                     @click="selectedOpen = false"
                   >
                     Cancel
+                  </v-btn>
+                  <v-btn text v-if="editing !== selectedEvent._id" @click.prevent="editEvent(selectedEvent)">Edit</v-btn>
+                  <v-btn text v-else @click="updateEvent(selectedEvent)">Save
                   </v-btn>
                 </v-card-actions>
               </v-card>
@@ -142,6 +145,9 @@ export default {
         console.log(error)
       }
     },
+    editEvent (ev) {
+      this.editing = ev._id
+    },
     viewDay ({ date }) {
       this.focus = date
       this.type = 'day'
@@ -175,48 +181,53 @@ export default {
       nativeEvent.stopPropagation()
     },
     updateRange ({ start, end }) {
-      const events = []
-
-      const min = new Date(`${start.date}T00:00:00`)
-      const max = new Date(`${end.date}T23:59:59`)
-      const days = (max.getTime() - min.getTime()) / 86400000
-      const eventCount = this.rnd(days, days + 20)
-
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-        const second = new Date(first.getTime() + secondTimestamp)
-
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: this.formatDate(first, !allDay),
-          end: this.formatDate(second, !allDay),
-          color: this.colors[this.rnd(0, this.colors.length - 1)]
-        })
-      }
-
       this.start = start
       this.end = end
-      this.events = events
     },
     nth (d) {
       return d > 3 && d < 21
         ? 'th'
         : ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'][d % 10]
+    }
+  },
+  computed: {
+    title () {
+      const { start, end } = this
+      if (!start || !end) {
+        return ''
+      }
+
+      const startMonth = this.monthFormatter(start)
+      const endMonth = this.monthFormatter(end)
+      const suffixMonth = startMonth === endMonth ? '' : endMonth
+
+      const startYear = start.year
+      const endYear = end.year
+      const suffixYear = startYear === endYear ? '' : endYear
+
+      const startDay = start.day + this.nth(start.day)
+      const endDay = end.day + this.nth(end.day)
+
+      switch (this.type) {
+        case 'month':
+          return `${startMonth} ${startYear}`
+        case 'week':
+        case '4day':
+          return `${startMonth} ${startDay} ${startYear} - ${suffixMonth} ${endDay} ${suffixYear}`
+        case 'day':
+          return `${startMonth} ${startDay} ${startYear}`
+      }
+      return ''
     },
-    rnd (a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a
-    },
-    formatDate (a, withTime) {
-      return withTime
-        ? `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()} ${a.getHours()}:${a.getMinutes()}`
-        : `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()}`
+    monthFormatter () {
+      return this.$refs.calendar.getFormatter({
+        timeZone: 'UTC', month: 'long'
+      })
     }
   },
   mounted () {
     this.getEvents()
+    this.$refs.calendar.checkChange()
   }
 }
 </script>
