0b739c520a993c976ebacecf18e8e187b4c7abec
bartek9105 <bartek9105@gmail.com>
2020-05-14 09:36:00 +0200

    update selected event

--- server/routes/event.js
+++ server/routes/event.js
@@ -5,5 +5,6 @@ const eventController = require('../controllers/eventController')
 
 router.get('/', eventController.getEvents)
 router.post('/add', eventController.addEvent)
+router.patch('/edit/:id', eventController.editEvent)
 
 module.exports = router
