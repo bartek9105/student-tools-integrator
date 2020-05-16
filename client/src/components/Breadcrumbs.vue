69732367646a41676443cea359256a29829f6f89
bartek9105 <bartek9105@gmail.com>
2020-05-16 22:41:56 +0200

    breadcrumbs

---
+++ client/src/components/Breadcrumbs.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <v-breadcrumbs>{{ this.$route.name }}</v-breadcrumbs>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Breadcrumbs'
+}
+</script>
+
+<style>
+
+</style>
