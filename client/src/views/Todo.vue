c3ea91083ae6a1adf444126e772a1cf6819abbcf
bartek9105 <bartek9105@gmail.com>
2020-05-17 16:27:10 +0200

    add todo list chart and data table

--- client/src/views/Todo.vue
+++ client/src/views/Todo.vue
@@ -1,11 +1,42 @@
 <template>
   <v-container>
+    <Breadcrumbs/>
+      <v-row>
+        <v-col cols="12" md="8">
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">Name</th>
+                  <th class="text-left">Calories</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>abc</td>
+                  <td>abc</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-col>
+        <v-col cols="12" md="4">
+          <Chart/>
+        </v-col>
+      </v-row>
   </v-container>
 </template>
 
 <script>
-export default {
+import Breadcrumbs from '@/components/Breadcrumbs'
+import Chart from '@/components/Chart'
 
+export default {
+  name: 'Todo',
+  components: {
+    Breadcrumbs,
+    Chart
+  }
 }
 </script>
 
