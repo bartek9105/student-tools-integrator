3db115c3ca39592030701c4e12aaa68872fba4a4
bartek9105 <bartek9105@gmail.com>
2020-05-17 21:52:40 +0200

    improve data tables and graph

--- client/src/views/Todo.vue
+++ client/src/views/Todo.vue
@@ -2,25 +2,188 @@
   <v-container>
     <Breadcrumbs/>
       <v-row>
+        <v-col cols="12" md="8" class="d-flex align-items-center">
+          <v-text-field
+            label="Add task"
+            required
+          ></v-text-field>
+          <v-btn color="primary">Add task</v-btn>
+        </v-col>
         <v-col cols="12" md="8">
-          <v-simple-table>
+          <v-simple-table class="mb-8 elevation-1">
             <template v-slot:default>
               <thead>
+                <span class="pt-6 pl-4">Tasks</span>
                 <tr>
+                  <th class="text-left">Done</th>
                   <th class="text-left">Name</th>
-                  <th class="text-left">Calories</th>
+                  <th class="text-left">Date</th>
+                  <th class="text-left">Status</th>
+                  <th class="text-left">Action</th>
                 </tr>
               </thead>
               <tbody>
+                <tr class="mt-2">
+                  <td>
+                    <v-icon>
+                      check_circle_outline
+                    </v-icon>
+                  </td>
+                  <td>abc</td>
+                  <td>abc</td>
+                  <td>
+                    <v-chip
+                      class="white--text"
+                      small
+                      color="green"
+                    >
+                      small chip
+                    </v-chip>
+                  </td>
+                  <td>
+                    <v-menu offset-y>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-on="on">
+                          more_vert
+                        </v-icon>
+                      </template>
+                      <v-list>
+                        <v-list-item>
+                          <v-list-item-title>Edit</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item>
+                          <v-list-item-title>Delete</v-list-item-title>
+                        </v-list-item>
+                      </v-list>
+                    </v-menu>
+                  </td>
+                </tr>
+                <tr class="mt-2">
+                  <td>
+                    <v-icon>
+                      check_circle_outline
+                    </v-icon>
+                  </td>
+                  <td>abc</td>
+                  <td>abc</td>
+                  <td>
+                    <v-chip
+                      class="white--text"
+                      small
+                      color="green"
+                    >
+                      small chip
+                    </v-chip>
+                  </td>
+                  <td>
+                    <v-menu offset-y>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-on="on">
+                          more_vert
+                        </v-icon>
+                      </template>
+                      <v-list>
+                        <v-list-item>
+                          <v-list-item-title>Edit</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item>
+                          <v-list-item-title>Delete</v-list-item-title>
+                        </v-list-item>
+                      </v-list>
+                    </v-menu>
+                  </td>
+                </tr>
+                <tr class="mt-2">
+                  <td>
+                    <v-icon>
+                      check_circle_outline
+                    </v-icon>
+                  </td>
+                  <td>abc</td>
+                  <td>abc</td>
+                  <td>
+                    <v-chip
+                      class="white--text"
+                      small
+                      color="green"
+                    >
+                      small chip
+                    </v-chip>
+                  </td>
+                  <td>
+                    <v-menu offset-y>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-on="on">
+                          more_vert
+                        </v-icon>
+                      </template>
+                      <v-list>
+                        <v-list-item>
+                          <v-list-item-title>Edit</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item>
+                          <v-list-item-title>Delete</v-list-item-title>
+                        </v-list-item>
+                      </v-list>
+                    </v-menu>
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+          <v-simple-table class="elevation-1">
+            <template v-slot:default>
+              <thead>
+                <span class="pt-6 pl-4">Tasks</span>
                 <tr>
+                  <th class="text-left">Done</th>
+                  <th class="text-left">Name</th>
+                  <th class="text-left">Date</th>
+                  <th class="text-left">Status</th>
+                  <th class="text-left">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="mt-2">
+                  <td>
+                    <v-icon>
+                      check_circle_outline
+                    </v-icon>
+                  </td>
                   <td>abc</td>
                   <td>abc</td>
+                  <td>
+                    <v-chip
+                      class="white--text"
+                      small
+                      color="green"
+                    >
+                      small chip
+                    </v-chip>
+                  </td>
+                  <td>
+                    <v-menu offset-y>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-on="on">
+                          more_vert
+                        </v-icon>
+                      </template>
+                      <v-list>
+                        <v-list-item>
+                          <v-list-item-title>Edit</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item>
+                          <v-list-item-title>Delete</v-list-item-title>
+                        </v-list-item>
+                      </v-list>
+                    </v-menu>
+                  </td>
                 </tr>
               </tbody>
             </template>
           </v-simple-table>
         </v-col>
-        <v-col cols="12" md="4">
+        <v-col cols="12" md="4" class="white elevation-1">
           <Chart/>
         </v-col>
       </v-row>
