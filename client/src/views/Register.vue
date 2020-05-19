7dc4af39408fa45698e0c32c985d7dfbe1a228db
bartek9105 <bartek9105@gmail.com>
2020-05-19 12:50:58 +0200

    register page

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,37 +1,49 @@
 <template>
-  <v-form>
-    <v-container>
-      <v-row class="flex-column align-center">
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="email"
-            label="E-mail"
-            required
-          ></v-text-field>
-        </v-col>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="password"
-            label="Password"
-            type="password"
-            required
-          ></v-text-field>
-        </v-col>
-        <p>Already registered?
-          <router-link to="login">
-            Login
-          </router-link>
-        </p>
-        <v-btn color="primary" @click="signUp">Register</v-btn>
-      </v-row>
-    </v-container>
-  </v-form>
+  <v-row>
+    <v-col cols=12 md=6 class="px-0 py-0">
+      <div class="hero-container">
+        <div class="hero"></div>
+        <div class="text">
+          <p class="display-1 white--text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, corporis. Fugit minus consectetur impedit? Rem?
+          </p>
+        </div>
+      </div>
+    </v-col>
+    <v-col cols=12 md=6 class="d-flex align-center justify-center">
+      <v-form>
+        <v-container>
+          <v-row class="flex-column align-center">
+            <v-col
+              cols="12"
+              md="12"
+            >
+              <div class="d-flex justify-center">
+                <v-icon color="blue">toll</v-icon>
+              </div>
+              <v-text-field
+                v-model="email"
+                label="E-mail"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                label="Password"
+                type="password"
+                required
+              ></v-text-field>
+            </v-col>
+            <p>Already registered?
+              <router-link to="login">
+                Login
+              </router-link>
+            </p>
+            <v-btn color="primary" @click="signUp">Register</v-btn>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -68,6 +80,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .hero-container {
+    position: relative;
+    .hero {
+      background-image: url('../assets/hero.jpg');
+      height: 100vh;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+    .text {
+      position: absolute;
+      top: 50%;
+    }
+  }
 </style>
