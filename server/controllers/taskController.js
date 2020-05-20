aaec146eae7920dc42b20bdddb1a9ed574a2b2ad
bartek9105 <bartek9105@gmail.com>
2020-05-20 10:16:39 +0200

    add task controller and routes

---
+++ server/controllers/taskController.js
@@ -0,0 +1,22 @@
+const Task = require('../models/Task')
+
+exports.getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find()
+        res.send(tasks)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.addTask = async (req, res) => {
+    try {
+        const newTask = await new Task({
+            name: req.body.name
+        })
+        const savedTask = await newTask.save()
+        res.send(savedTask)        
+    } catch (error) {
+        console.log(error)
+    }
+}
