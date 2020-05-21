aa256981bcbe2c6c45a2a58ab033a1b445c1c1df
bartek9105 <bartek9105@gmail.com>
2020-05-21 21:35:16 +0200

    display task for the day on dashboard view

---
+++ client/src/components/TasksToday.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container>
+        <div class="d-flex align-center mb-3">
+            <h3 class="mr-2">Your tasks for today</h3>
+        </div>
+        <v-simple-table>
+            <template v-slot:default>
+            <thead>
+                <tr>
+                <th class="text-left">Task</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="task in getTodaysTasks" :key="task._id">
+                <td>
+                    {{ task.name }}
+                    </td>
+                </tr>
+            </tbody>
+            </template>
+        </v-simple-table>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TasksToday',
+  computed: {
+    getTodaysTasks () {
+      return this.$store.getters.getTasks.filter(task => {
+        return task.dueDate === new Date().toISOString().substr(0, 10)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -4,17 +4,22 @@
       <v-col cols="12" md="6">
         <UpcomingEvents/>
       </v-col>
+      <v-col cols="12" md="6">
+        <TasksToday/>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
 import UpcomingEvents from '@/components/UpcomingEvents'
+import TasksToday from '@/components/TasksToday'
 
 export default {
   name: 'Dashboard',
   components: {
-    UpcomingEvents
+    UpcomingEvents,
+    TasksToday
   }
 }
 </script>
