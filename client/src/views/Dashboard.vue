4d0893758ff50dad0d690ff032843a1d989844e7
bartek9105 <bartek9105@gmail.com>
2020-05-21 13:14:17 +0200

    abstract todo list to new component, import todo to dashboard

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,16 +1,25 @@
 <template>
   <v-container>
-    <UpcomingEvents/>
+    <v-row>
+      <v-col cols="12" md="6">
+        <UpcomingEvents/>
+      </v-col>
+      <v-col cols="12" md="6">
+        <TodoList/>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 import UpcomingEvents from '@/components/UpcomingEvents'
+import TodoList from '@/components/TodoList'
 
 export default {
   name: 'Dashboard',
   components: {
-    UpcomingEvents
+    UpcomingEvents,
+    TodoList
   }
 }
 </script>
