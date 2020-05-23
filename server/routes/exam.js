be35c2a07d0d52bbfd19d97c24d706d9bedf70df
bartek9105 <bartek9105@gmail.com>
2020-05-23 16:23:18 +0200

    edit/delete exam endpoints, client connection and notifications

--- server/routes/exam.js
+++ server/routes/exam.js
@@ -6,5 +6,7 @@ const examController = require('../controllers/examController')
 
 router.get('/', examController.getExams)
 router.post('/add', examController.addExam)
+router.patch('/:id/edit', examController.editExam)
+router.delete('/:id/delete', examController.deleteExam)
 
 module.exports = router
