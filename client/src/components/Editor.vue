7f90b86fe6ddfd655fa9519568060eb9de03c503
bartek9105 <bartek9105@gmail.com>
2020-05-28 22:49:57 +0200

    add wysiwyg editor, add endpoints for notes

---
+++ client/src/components/Editor.vue
@@ -0,0 +1,52 @@
+<template>
+  <quill-editor
+    ref="myQuillEditor"
+    :options="editorOption"
+    @blur="onEditorBlur($event)"
+    @focus="onEditorFocus($event)"
+    @ready="onEditorReady($event)"
+  />
+</template>
+
+<script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
+
+export default {
+  name: 'Editor',
+  components: {
+    quillEditor
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    onEditorBlur (quill) {
+      console.log('editor blur!', quill)
+    },
+    onEditorFocus (quill) {
+      console.log('editor focus!', quill)
+    },
+    onEditorReady (quill) {
+      console.log('editor ready!', quill)
+    },
+    onEditorChange ({ quill, html, text }) {
+      console.log('editor change!', quill, html, text)
+      this.content = html
+    }
+  },
+  computed: {
+    editor () {
+      return this.$refs.myQuillEditor.quill
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- server/models/Subject.js
+++ server/models/Subject.js
@@ -9,7 +9,8 @@ const subjectSchema = new mongoose.Schema({
     exams: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Exam'
-    }
+    },
+    notes: [String]
 })
 
 module.exports = mongoose.model('Subject', subjectSchema)
