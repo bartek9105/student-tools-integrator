16f85392ca322602ecaf996a42cb1de6101b8d60
bartek9105 <bartek9105@gmail.com>
2020-05-28 13:07:50 +0200

    add new subject feature when adding new class to schedule

--- client/src/store/subjects.js
+++ client/src/store/subjects.js
@@ -12,12 +12,20 @@ export default ({
   mutations: {
     SET_SUBJECTS (state, subjects) {
       state.subjects = subjects
+    },
+    ADD_SUBJECT (state, subject) {
+      state.subjects.push(subject)
     }
   },
   actions: {
     fetchSubjects ({ commit }) {
       return axios.get('http://localhost:3000/subjects').then(res => {
         commit('SET_SUBJECTS', res.data)
+      }).catch(err => console.log(err))
+    },
+    addSubject ({ commit }, payload) {
+      axios.post('http://localhost:3000/subjects/add', payload).then(result => {
+        commit('ADD_SUBJECT', result.data.subject)
       }).catch(err => console.log(err))
     }
   }
