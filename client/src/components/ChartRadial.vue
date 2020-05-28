ee21adb6e114a8d27ee55a3af2f5199f28ffc37a
bartek9105 <bartek9105@gmail.com>
2020-05-28 22:24:40 +0200

    add progress chart, styles for files list, basic grid

---
+++ client/src/components/ChartRadial.vue
@@ -0,0 +1,51 @@
+<template>
+   <div id="chart">
+      <apexchart type="radialBar" height="300" :options="chartOptions" :series="series"></apexchart>
+    </div>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+  name: 'ChartRadial',
+  components: {
+    apexchart: VueApexCharts
+  },
+  data () {
+    return {
+      series: [44, 55, 67, 83],
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: 'radialBar'
+        },
+        plotOptions: {
+          radialBar: {
+            dataLabels: {
+              name: {
+                fontSize: '22px'
+              },
+              value: {
+                fontSize: '16px'
+              },
+              total: {
+                show: true,
+                label: 'Total',
+                formatter: function (w) {
+                  return 249
+                }
+              }
+            }
+          }
+        },
+        labels: ['Apples', 'Oranges', 'Bananas', 'Berries']
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
