ade5cb54571328ec6d3a06e9d2c0d5bf51a5f4ef
bartek9105 <bartek9105@gmail.com>
2020-05-29 20:40:46 +0200

    fix

--- server/controllers/subjectController.js
+++ server/controllers/subjectController.js
@@ -55,7 +55,7 @@ exports.deleteRequirement = async (req, res) => {
 
 exports.getSubjects = async (req, res) => {
     try {
-        const subjects = await Subject.find()
+        const subjects = await Subject.find().populate('exams', 'name')
         res.send(subjects)       
     } catch (error) {
         console.log(error)
