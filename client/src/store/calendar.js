7da69ad24b88fd3d4424e6530e22acc2405564be
bartek9105 <bartek9105@gmail.com>
2020-06-01 10:22:30 +0200

    add token verification for all routes

--- client/src/store/calendar.js
+++ client/src/store/calendar.js
@@ -19,14 +19,14 @@ export default ({
   },
   actions: {
     getEvents ({ commit }) {
-      return Api.get('events').then(events => {
+      return Api().get('events').then(events => {
         commit('SET_EVENTS', events.data)
       }).catch(err => {
         console.log(err)
       })
     },
     addEvent ({ commit }, payload) {
-      Api.post('events/add', payload).then(result => {
+      Api().post('events/add', payload).then(result => {
         commit('ADD_EVENT', result.data.event)
       }).catch(err => console.log(err))
     }
--- server/routes/exam.js
+++ server/routes/exam.js
@@ -3,10 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 const examController = require('../controllers/examController')
+const isAuth = require('../policies/isAuth')
 
-router.get('/', examController.getExams)
-router.post('/add', examController.addExam)
-router.patch('/:id/edit', examController.editExam)
-router.delete('/:id/delete', examController.deleteExam)
+router.get('/', isAuth, examController.getExams)
+router.post('/add', isAuth, examController.addExam)
+router.patch('/:id/edit', isAuth, examController.editExam)
+router.delete('/:id/delete', isAuth, examController.deleteExam)
 
 module.exports = router
