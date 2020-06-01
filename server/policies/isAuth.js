c0b32e64196f708cce1f6183cec750a46ec2c4ce
bartek9105 <bartek9105@gmail.com>
2020-06-01 10:22:30 +0200

    add token verification for all routes

--- server/policies/isAuth.js
+++ server/policies/isAuth.js
@@ -1,5 +1,20 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
-
+    const authHeader = req.get('Authorization')
+    if (!authHeader) {
+        res.status(401).send('Not authenticated')
+    } 
+    const token = authHeader.split(' ')[1]
+    let decodedToken
+    try {
+        decodedToken = jwt.verify(token, 'secretsecretsecretkey')
+    } catch (error) {
+        console.log(error)
+    }
+    if (!decodedToken) {
+        res.send('Not authenticated')
+    }
+    req.userId = decodedToken.userId
+    next()
 }
--- server/routes/event.js
+++ server/routes/event.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const eventController = require('../controllers/eventController')
+const isAuth = require('../policies/isAuth')
 
-router.get('/', eventController.getEvents)
-router.post('/add', eventController.addEvent)
-router.patch('/edit/:id', eventController.editEvent)
-router.delete('/delete/:id', eventController.deleteEvent)
+router.get('/', isAuth, eventController.getEvents)
+router.post('/add', isAuth, eventController.addEvent)
+router.patch('/edit/:id', isAuth, eventController.editEvent)
+router.delete('/delete/:id', isAuth, eventController.deleteEvent)
 
 module.exports = router
