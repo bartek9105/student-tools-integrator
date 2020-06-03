dce2cbe5daddd16089edcbaf8c8900691345f082
bartek9105 <bartek9105@gmail.com>
2020-06-03 22:23:29 +0200

    fix indentation

--- client/src/components/TasksToday.vue
+++ client/src/components/TasksToday.vue
@@ -1,25 +1,23 @@
 <template>
-    <v-container>
-        <div class="d-flex align-center mb-3">
-            <h3 class="mr-2">Your tasks for today</h3>
-        </div>
-        <v-simple-table>
-            <template v-slot:default>
-            <thead>
-                <tr>
-                <th class="text-left">Task</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="task in getTodaysTasks" :key="task._id">
-                <td>
-                    {{ task.name }}
-                    </td>
-                </tr>
-            </tbody>
-            </template>
-        </v-simple-table>
-    </v-container>
+  <v-container>
+    <div class="d-flex align-center mb-3">
+        <h3 class="mr-2">Your tasks for today</h3>
+    </div>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Task</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="task in getTodaysTasks" :key="task._id">
+            <td>{{ task.name }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
 </template>
 
 <script>
