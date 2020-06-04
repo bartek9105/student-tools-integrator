ba979e52409f626991b3c59f4e9f1f148ab44e4c
bartek9105 <bartek9105@gmail.com>
2020-06-04 12:00:35 +0200

    styling fixes and improvements for login/register view

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,29 +1,34 @@
 <template>
-  <div class="container">
-    <v-row>
-      <v-col cols="12" md="3">
-        <v-form class="text-center">
-          <div class="d-flex justify-center">
-            <v-icon color="blue">toll</v-icon>
+  <div class="container d-flex align-center" style="height: 100vh;">
+    <v-row class="justify-center">
+      <v-col cols="12" md="4">
+        <div class="auth-form-container elevation-3">
+          <div style="background-color: #1565C0;" class="white--text py-8 px-4 text-center">
+            <p class="mb-0">Student Tools Integrator</p>
           </div>
-          <v-text-field
-            v-model="email"
-            label="E-mail"
-            required
-          ></v-text-field>
-          <v-text-field
-            v-model="password"
-            label="Password"
-            type="password"
-            required
-          ></v-text-field>
-          <p>Already registered?
-            <router-link to="register">
-              Signup
-            </router-link>
-          </p>
-          <v-btn color="primary" @click="signIn">Login</v-btn>
-        </v-form>
+          <v-form class="text-center px-8 py-8">
+            <div class="d-flex justify-center">
+              <v-icon color="blue">toll</v-icon>
+            </div>
+            <v-text-field
+              v-model="email"
+              label="E-mail"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="Password"
+              type="password"
+              required
+            ></v-text-field>
+            <p class="mt-4 mb-4">Don't have an account?
+              <router-link to="register">
+                Signup
+              </router-link>
+            </p>
+            <v-btn color="primary" @click="signIn">Login</v-btn>
+          </v-form>
+        </div>
       </v-col>
     </v-row>
   </div>
@@ -62,5 +67,13 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@media screen and (min-width: 960px) {
+  .container {
+    padding: 0;
+  }
+}
+.auth-form-container {
+  background-color: #fff;
+}
 </style>
