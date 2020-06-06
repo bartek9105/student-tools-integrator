4e81cbc1ea47421750c7ffad36186a09fcf580ad
bartek9105 <bartek9105@gmail.com>
2020-06-06 13:57:38 +0200

    deploy

---
+++ server/public/precache-manifest.7072bd3de1d14357c87fc60e105fd48c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7afdc75b051087d54f",
+    "url": "/css/app.16884a93.css"
+  },
+  {
+    "revision": "34e2a39df6880397cad0",
+    "url": "/css/chunk-vendors.7dc374c5.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "ecc0802427b5d762444b53f0bde68875",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c7afdc75b051087d54f",
+    "url": "/js/app.110ed2b4.js"
+  },
+  {
+    "revision": "34e2a39df6880397cad0",
+    "url": "/js/chunk-vendors.50f02906.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
