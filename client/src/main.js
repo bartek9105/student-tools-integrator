600b8b0883b906fd98691fafc1e8fa3786c4cbcb
bartek9105 <bartek9105@gmail.com>
2020-06-08 15:48:35 +0200

    keep user logged in after page reload

--- client/src/main.js
+++ client/src/main.js
@@ -13,6 +13,11 @@ new Vue({
   store,
   vuetify,
   created () {
+    const userString = localStorage.getItem('user')
+    if (userString) {
+      const userData = JSON.parse(userString)
+      this.$store.commit('SET_USER_DATA', userData)
+    }
     this.$store.dispatch('fetchSubjects')
     this.$store.dispatch('fetchTasks')
     this.$store.dispatch('getEvents')
